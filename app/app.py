12bed4d3aab52b1b5b66b8ecc7e0b3b51f622705
eloco <exia@isimarkets.com>
2022-04-23 16:52:56 +0800

    modify

--- app/app.py
+++ app/app.py
@@ -8,33 +8,41 @@ import base64
 import os,sys
 import datetime,time,random
 import httpx,requests
+import traceback
 
 result = """ u need to send param  event[run] with base64 encode """
 
 @timeout(60*10) # 10 minutes
 def handler(event, context):
-    with sync_playwright() as playwright:
-        global result
-        if "browser" in event:
-            if   event["browser"].lower() == "chromium":
-                browser = playwright.chromium.launch(headless=True)
-            elif event["browser"].lower() == "firefox":
-                browser = playwright.firefox.launch(headless=True)
-            else:
-                browser = playwright.webkit.launch(headless=True)
-            context = browser.new_context()
-            page    = context.new_page()
+    try:
+        with sync_playwright() as playwright:
+            global result
+            if "browser" in event:
+                if   event["browser"].lower() == "chromium":
+                    browser = playwright.chromium.launch(headless=True)
+                elif event["browser"].lower() == "firefox":
+                    browser = playwright.firefox.launch(headless=True)
+                else:
+                    browser = playwright.webkit.launch(headless=True)
+                context = browser.new_context()
+                page    = context.new_page()
 
-        run="global result;"
-        if "run" in event.keys():
-            try:
-                run+=base64.b64decode(event["run"]).decode('utf-8')
-            except:
-                run+=event["run"]
+            run="global result;"
+            if "run" in event.keys():
+                try:
+                    run+=base64.b64decode(event["run"]).decode('utf-8')
+                except:
+                    run+=event["run"]
 
-        exec(run)
+            print(run)
+            exec(run)
 
-        return {
-            'statusCode': 200,
-            'body': result,
-        }
+    except Exception as e:
+        result=str(e)
+        traceback.print_exc()
+        status_code = 500
+
+    return {
+        'code': status_code,
+        'result': result,
+    }
